a41a89f834107216413163903480f709e12cf0dd
elaris6 <elaris6@gmail.com>
2021-12-06 16:27:22 +0100

    Modificación de prueba en la clase test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,14 +1,17 @@
 package com.paymentchain.billing;
 
+import or.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
+	String mensaje = "Mensaje añadido test Jenkins";
+	Assert.assertNotNull(mensaje);
 	@Test
 	public void contextLoads() {
 	}
